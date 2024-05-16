e5bddf8f0611d82a7922c489ae8d6dbd5f29a952
helx-20 <129262470+helx-20@users.noreply.github.com>
2024-05-16 22:12:32 +0800

    Delete ACC_0505 directory

--- ACC_0505/check_data.py
+++
@@ -1,7 +0,0 @@
-import os
-name_list = os.listdir('/mnt/mnt1/linxuan/nnv/ACC/new_train_data2_critical_json')
-for i in range(126036):
-    if 'Rc_{}.json'.format(i+1) not in name_list:
-        print('Rc_{}.json'.format(i+1))
-    if 'Uin_{}.json'.format(i+1) not in name_list:
-        print('Uin_{}.json'.format(i+1))
